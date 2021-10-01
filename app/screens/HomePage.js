abdaf48973873d2d3623ef5ae6d52f6af7cf2efa
James Ross <james_ross@outlook.fr>
2021-10-01 14:09:26 +0200

    fooooools

--- app/screens/HomePage.js
+++ app/screens/HomePage.js
@@ -50,7 +50,7 @@ useEffect(() => {
 
   const alreadyLoggedIn = (e) => {
     Alert.alert("You're already logged in");
-    console.log("You're already logged in, fool!");
+    console.log("You're already logged in, foooooool!");
   };
 
   const loginUser = () => {
